aeca617ea26e1296613e43f1fcf73efb1b62c123
0109-03 <0109-03@0109-03>
2024-05-04 17:53:01 +0300

    Р”РѕР±Р°РІСЊС‚Рµ С„Р°Р№Р»С‹ РїСЂРѕРµРєС‚Р°.

---
+++ AppConfig.cs
@@ -0,0 +1,22 @@
+﻿namespace TestAPI
+{
+    public class AppConfig
+    {
+        private readonly IConfiguration _config;
+        private int myProperty;
+
+        public AppConfig(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string ConnectionString
+        {
+            get
+            {
+                return _config.GetConnectionString("MysqlDefaultConnection") ?? "";
+            }
+        }
+    }
+}
+
